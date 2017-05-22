5167ec9924771f4cb94b045cf8ec45dafbfcfb42
Marius Bogoevici <mbogoevici@pivotal.io>
2017-05-22 19:33:35 -0400

    Initial commit of an example reproducing the reported issue

---
+++ src/main/java/com/example/jasypt/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.jasypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.StandardEnvironment;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class,
+				"--spring.profiles.active=other");
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(NoBeanContext.class);
+		builder.parent(context);
+		StandardEnvironment environment = new StandardEnvironment();
+		environment.merge(context.getEnvironment());
+		builder.environment(context.getEnvironment());
+		builder.run();
+	}
+
+	public static class NoBeanContext {
+
+	}
+}
