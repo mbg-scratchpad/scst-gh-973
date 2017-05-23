e904e81238d0b745b2c911c332ba20707fe2bb80
Marius Bogoevici <mbogoevici@pivotal.io>
2017-05-23 11:22:26 -0400

    Update with simpler property source example

--- src/main/java/com/example/jasypt/EnvironmentIteratingApplication.java
+++ src/main/java/com/example/jasypt/EnvironmentIteratingApplication.java
@@ -4,20 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EnvironmentIteratingApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class,
+		ConfigurableApplicationContext context = SpringApplication.run(EnvironmentIteratingApplication.class,
 				"--spring.profiles.active=other");
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(NoBeanContext.class);
-		builder.parent(context);
-		StandardEnvironment environment = new StandardEnvironment();
-		environment.merge(context.getEnvironment());
-		builder.environment(context.getEnvironment());
-		builder.run();
+		for (PropertySource<?> propertySource : context.getEnvironment().getPropertySources()) {
+			propertySource.getProperty("encrypted.value");
+		}
 	}
 
 	public static class NoBeanContext {
